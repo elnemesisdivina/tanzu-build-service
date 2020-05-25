5f832b2392d450631cdb6545cd1beb64a6560242
James Musselwhite <jmusselwhite@pivotal.io>
2020-05-25 12:37:34 -0700

    cleaning up error output in cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-pb image delete index.docker.io/techgnosis/mysql
-pb builder delete demo-builder --cluster
-pb builder delete default --cluster
+pb image delete index.docker.io/techgnosis/mysql 2> /dev/null
+pb builder delete demo-builder --cluster 2> /dev/null
+pb builder delete default --cluster 2> /dev/null
 
 # Requires you to be logged into registry.pivotal.io via Docker
 pb stack update --build-image registry.pivotal.io/tbs-dependencies/build:1584989900 --run-image registry.pivotal.io/tbs-dependencies/run:1584989900
