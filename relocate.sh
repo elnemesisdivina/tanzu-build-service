6b1c27443fef8f6312308fc5ad0b602f1dd29f6f
James Musselwhite <jmusselwhite@pivotal.io>
2020-06-24 14:25:12 -0700

    Removing username and password from relocate

--- relocate.sh
+++ relocate.sh
@@ -4,6 +4,10 @@
 # If you clear out that folder you can still use relocated images
 # It takes forever to upload to DockerHub so this matters
 
+#REGISTRY="index.docker.io/your-username"
+REGISTRY="gcr.io/your-project/build-service"
+
 duffle relocate -f ./build-service-0.1.0.tgz \
 -m ./relocated.json \
--p index.docker.io/techgnosis
+-p "${REGISTRY}"
+
