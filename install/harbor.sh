1b6632522b728e42e01b672e7852b42fea57b0d8
James Musselwhite <jmusselwhite@pivotal.io>
2020-06-30 11:24:14 -0700

    Separating out install variables

---
+++ install/harbor.sh
@@ -0,0 +1,5 @@
+DOCKER_REPOSITORY="harbor2.lab.home:30003/tbs"
+DOCKER_REGISTRY="harbor2.lab.home:30003"
+REGISTRY_USERNAME=admin
+REGISTRY_PASSWORD="Harbor12345"
+CUSTOM_BUILDER_IMAGE="harbor2.lab.home:30003/tbs/tbs-default-cluster-builder"
