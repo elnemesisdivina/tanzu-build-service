15a7d1622995e8e16de1f667f5c6fc2b522d6d40
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-30 16:24:29 -0700

    erroneous old comment

--- secrets/harbor.sh
+++ secrets/harbor.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-# This will prompt for the password
 REGISTRY_PASSWORD=Harbor12345 kp secret create harbor2-creds \
 --namespace tbs-demo \
 --registry harbor.lab.home \
