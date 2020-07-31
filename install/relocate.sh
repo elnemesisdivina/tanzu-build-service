b79afe81b051d744fed9432e0fbdf760c1643aa0
James Musselwhite <jmusselwhite@pivotal.io>
2020-07-31 09:27:21 -0700

    just using library

--- install/relocate.sh
+++ install/relocate.sh
@@ -2,7 +2,7 @@
 
 #REGISTRY="index.docker.io/your-username"
 #REGISTRY="gcr.io/your-project/build-service"
-REGISTRY="harbor.lab.home/tbs"
+REGISTRY="harbor.lab.home/library"
 
 duffle relocate -f ./build-service-0.2.0.tgz \
 -m ./relocated.json \
